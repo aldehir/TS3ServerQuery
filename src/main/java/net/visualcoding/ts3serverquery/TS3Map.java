20198e4acf44f9a207589c3b838b8d4242011dcf
Aldehir Rojas <aldehir@visualcoding.net>
2012-08-23 19:20:07 -0500

    Implement TS3Map and TS3MapTest

--- src/main/java/net/visualcoding/ts3serverquery/TS3Map.java
+++ src/main/java/net/visualcoding/ts3serverquery/TS3Map.java
@@ -3,41 +3,200 @@ package net.visualcoding.ts3serverquery;
 import java.util.Map;
 import java.util.Set;
 import java.util.List;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.lang.StringBuilder;
 
+/**
+ * This class provides a map structure suitable for TS3 Server Query queries.
+ * 
+ * @author Aldehir Rojas
+ * @version 1.0
+ */
 public class TS3Map {
 
-    Map<String, List<String>> map;
-    
+    /** Map used to store the mappings */
+    private Map<String, List<String>> map;
+   
+    /**
+     * Constructs an empty map.
+     */
     public TS3Map() {
         // Instantiate the map object to contain values
         map = new HashMap<String, List<String>>();
     }
 
+    /**
+     * Constructs a map and initializes it to the map representation of
+     * {@code mapString}.
+     *
+     * @param mapString String representation of the TS3Map.
+     */
     public TS3Map(String mapString) {
         // Parse string
         map = parseMap(mapString);
     }
 
-    public boolean add(String name, String value) {
-        // Trim name/values
-        name = name.trim();
+    /**
+     * Returns the {@code String} object to which the specified key is mapped,
+     * or null if this map contains no mapping for the key.
+     * 
+     * @param key the key whose associated value is to be returned
+     * @return the {@code String} object to which the specified key is mapped,
+     *         or null if this map contains no mapping for the key
+     */
+    public String get(String key) {
+        List<String> list = map.get(key);
+
+        if(list == null || list.isEmpty()) return null;
+        return ((LinkedList<String>)list).getFirst();
+    }
+
+    /**
+     * Returns the {@code Integer} object to which the specified key is mapped,
+     * or null if this map contains no mapping for the key or if the value
+     * associated with the key is not a parsable integer.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the {@code Integer} object to which the specified key is mapped,
+     *         or null if this map contains no mapping for the key or if the
+     *         value associated with the key is not a parsable integer
+     */
+    public Integer getInteger(String key) {
+        String strValue = get(key);
+        if(strValue == null) return null;
+
+        Integer integer = null;
+        try {
+            integer = Integer.valueOf(strValue);
+        } catch(NumberFormatException e) {
+            return null;
+        }
+
+        return integer;
+    }
+
+
+    /**
+     * Returns the {@code List<String>} object to which the specified key is
+     * mapped, or null if this map contains no mapping for the key.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the {@code List<String>} object to which the specified key is
+     *         mapped, or null if this map contains no mapping for the key
+     */
+    public List<String> getList(String key) {
+        if(!contains(key)) return null;
+        return new LinkedList<String>(map.get(key));
+    }
+
+    /**
+     * Returns the list to which the specified key is mapped where the elements
+     * are parsed to an Integer object, or null if this map contains no mapping
+     * for the key. Elements that are not parsable integers are not included
+     * in the list.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the list to which the specified key is mapped where the elements
+     *         are parsed to an Integer object, or null if this map contains no
+     *         mapping for the key
+     */
+    public List<Integer> getIntegerList(String key) {
+        if(!contains(key)) return null;
+
+        // Create a list of integers
+        List<Integer> list = new LinkedList<Integer>();
+        List<String> stringList = map.get(key);
+
+        if(stringList == null) return null;
+
+        for(String item : stringList) {
+            try {
+                list.add(Integer.valueOf(item));
+            } catch(NumberFormatException e) {
+                // Ignore
+            }
+        }
+
+        // Return null if list is empty
+        if(list.isEmpty()) return null;
+
+        // Otherwise return the list
+        return list;
+    }
+
+    /**
+     * Returns {@code true} if this map contains {@code key} and the value of
+     * {@code key} is a list.
+     * @return {@code true} if this map contains {@code key} and the value of
+     *         {@code key} is a list
+     */
+    public boolean isList(String key) {
+        List<String> list = map.get(key);
+        
+        // Only consider it a list if it has more than 1 item
+        if(list == null || list.size() <= 1) return false;
+        return true;
+    }
+
+    /**
+     * Returns {@code true} if this map contains {@code key} and the value of
+     * {@code key} is an integer.
+     * @return {@code true} if this map contains {@code key} and the value of
+     *         {@code key} is an integer
+     */
+    public boolean isInteger(String key) {
+        String value = get(key);
+        if(value == null) return false;
+
+        try {
+            Integer.parseInt(value);
+        } catch(NumberFormatException e) {
+            // If we can't parse it, then it must not be an integer
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns {@code true} if this map contains {@code key} and {@code key} is
+     * a switch.
+     * @return {@code true} if this map contains {@code key} and {@code key} is
+     *         a switch
+     */
+    public boolean isSwitch(String key) {
+        return contains(key) && get(key) == null;
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map. If
+     * the map already contains the specified key, then the specified value is
+     * appended to the list associated with the specified key.
+     * 
+     * @param key   key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return {@code true} if the mapping was successfully added
+     */
+    public boolean add(String key, String value) {
+        // Trim key/values
+        key = key.trim();
         value = value.trim();
 
-        // Don't add if the name or value variables are empty
-        if(name.isEmpty() || value.isEmpty()) return false;
+        // Don't add if the key or value variables are empty
+        if(key.isEmpty() || value.isEmpty()) return false;
         
-        // Get the list of the given key/name
-        List<String> list = map.get(name);
+        // Get the list of the given key
+        List<String> list = map.get(key);
 
-        // If key doesn't exist, then instantiate a list and add it under the
-        // key
+        // If key doesn't exist, then instantiate a list and
+        // add it under the key
         if(list == null) {
-            list = new List<String>();
-            map.put(name, list);
+            list = new LinkedList<String>();
+            map.put(key, list);
         }
 
         // Add value to the list
@@ -46,10 +205,76 @@ public class TS3Map {
         return true;
     }
 
-    public boolean addInteger(String name, int integer) {
-        return add(name, Integer.toString(integer));
+    /**
+     * Associates the specified integer value with the specified key in this
+     * map. If the map already contains the specified key, then the specified
+     * value is appended to the list associated with the specified key.
+     *
+     * @param key   key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return {@code true} if the mapping was successfully added
+     */
+    public boolean addInteger(String key, int integer) {
+        return add(key, Integer.toString(integer));
     }
 
+    /**
+     * Associates the specified key as a switch in this map.
+     *
+     * @param key key which is to be associated with a switch
+     * @return {@code true} if the mapping was successfully added,
+     *         {@code false} if the map already contains the specified key
+     *         and is not a switch.
+     */
+    public boolean addSwitch(String key) {
+        List<String> value = map.get(key);
+
+        // If there is no mapping, or if the value is null then
+        // add to the map and return true
+        if(value == null) {
+            map.put(key, null);
+            return true;
+        }
+
+        // Otherwise return false
+        return false;
+    }
+
+    /**
+     * Returns {@code true} if this map contains {@code key}.
+     * @return {@code true} if this map contains {@code key}
+     */
+    public boolean contains(String key) {
+        return map.containsKey(key);
+    }
+
+    /**
+     * Removes the mapping of the specified key in this map.
+     * @return {@code true} if the key exists and was removed
+     */
+    public boolean remove(String key) {
+        // Return false if there is no mapping
+        if(!map.containsKey(key)) return false;
+
+        // Otherwise, remove the mapping and return true
+        map.remove(key);
+        return true;
+    }
+
+    /**
+     * Removes all the mappings in this map.
+     */
+    public void clear() {
+        map.clear();
+    }
+
+    /**
+     * Parses the specified string representation of a TS3Map.
+     * 
+     * @param mapString String representation of a TS3Map
+     * @return Internal map object used by TS3Map containing the mappings in the
+     *         specified string.
+     */
     protected Map<String, List<String>> parseMap(String mapString) {
         // Instantiate a map
         Map<String, List<String>> map = new HashMap<String, List<String>>();
@@ -68,10 +293,26 @@ public class TS3Map {
         return map;
     }
 
+    /**
+     * Parses the specified string representation of an entry in a TS3Map.
+     * 
+     * @param entry String representation of an entry for TS3Map
+     * @return A Map.Entry object containing the parsed contents of the
+     *         specified string.
+     */
     protected Map.Entry<String, List<String>> parseMapEntry(String entry) {
         // Trim whitespace and return null if empty
         entry = entry.trim();
         if(entry.isEmpty()) return null;
+
+        // Check if this is a switch
+        if(entry.startsWith("-")) {
+            // Get the rest of the string
+            String switchName = entry.substring(1);
+
+            // Return a map entry with the value as null, implying a switch
+            return new AbstractMap.SimpleEntry(switchName, null);
+        }
 
         String name = null; // Name
         List<String> pairs = new LinkedList<String>();  // List of pairs
@@ -104,5 +345,58 @@ public class TS3Map {
 
         // Return map entry
         return new AbstractMap.SimpleEntry<String, List<String>>(name, values);
+    }
+
+    /**
+     * Returns a string representation of a Map.Entry object.
+     * @return a string representation of a Map.Entry object.
+     */
+    protected String entryToString(Map.Entry<String, List<String>> entry) {
+        StringBuilder sb = new StringBuilder();
+
+        if(entry.getValue() == null) {
+            // Handle switches
+            sb.append('-');
+            sb.append(entry.getKey());
+
+            return sb.toString();
+        }
+
+        // Get the list
+        List<String> list = entry.getValue();
+
+        // Iterate through the values and add each to our string
+        Iterator<String> it = list.iterator();
+        while(it.hasNext()) {
+            sb.append(entry.getKey());
+            sb.append('=');
+            sb.append(TS3Util.escape(it.next()));
+
+            // Add a pipe if there are more values
+            if(it.hasNext()) sb.append('|');
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * Returns a string representation of this map.
+     * @return a string representation of this map
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        // Get an iterator for our map
+        Iterator<Map.Entry<String, List<String>>> it;
+        it = map.entrySet().iterator();
+
+        while(it.hasNext()) {
+            sb.append(entryToString(it.next()));
+
+            // Add space if there are more entries
+            if(it.hasNext()) sb.append(' ');
+        }
+
+        return sb.toString();
     }
 }
--- src/test/java/net/visualcoding/ts3serverquery/TS3MapTest.java
+++ src/test/java/net/visualcoding/ts3serverquery/TS3MapTest.java
@@ -6,6 +6,7 @@ import junit.framework.TestSuite;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
 
 /**
  * Unit test for TS3Map
@@ -35,7 +36,7 @@ public class TS3MapTest extends TestCase {
         // Create a map object
         TS3Map map = new TS3Map();
 
-        // Test the parseMapEntry method
+        // Test the parseMapEntry() method
         Map.Entry<String, List<String>> entry = null;
 
         entry = map.parseMapEntry("name=value");
@@ -48,16 +49,117 @@ public class TS3MapTest extends TestCase {
         assertEquals(entry.getValue().size(), 4);
         String[] values = {"val1", "val2", "val3", "val4"};
         for(String value : values) assertTrue(entry.getValue().contains(value));
+        
+        entry = map.parseMapEntry("-switch");
+        assertEquals(entry.getKey(), "switch");
+        assertNull(entry.getValue());
 
-        // Test the parseMap method
+        // Test the parseMap() method
         Map<String, List<String>> m = null;
 
-        m = map.parseMap("name=value entry=val1|entry=val2|entry=val3");
-        assertEquals(m.size(), 2);
+        m = map.parseMap("name=value entry=val1|entry=val2|entry=val3 -switch");
+        assertEquals(m.size(), 3);
         assertTrue(m.containsKey("name"));
         assertTrue(m.containsKey("entry"));
+        assertTrue(m.containsKey("switch"));
+        assertNull(m.get("switch"));
 
-        assertTrue( true );
+        // Test the entryToString() method
+        values = new String[] { "-switch", "name=value", "name=v1|name=v2" };
+        for(String value : values) {
+            entry = map.parseMapEntry(value);
+            assertEquals(map.entryToString(entry), value);
+        }
+
+        // Test the toString() method
+        map = new TS3Map("name=value -switch");
+
+        // Since we're using a HashMap, the order may not be the same.
+        boolean valid = false;
+        if(map.toString().equals("name=value -switch")) valid = true;
+        if(map.toString().equals("-switch name=value")) valid = true;
+        assertTrue(valid);
+
+        // Test the add methods
+        map.clear();
+        map.add("name", "value");
+        assertEquals(map.toString(), "name=value");
+
+        map.clear();
+        map.add("entry", "v1");
+        map.add("entry", "v2");
+        map.add("entry", "v3");
+        assertEquals(map.toString(), "entry=v1|entry=v2|entry=v3");
+
+        map.clear();
+        map.addInteger("client_id", 1);
+        assertEquals(map.toString(), "client_id=1");
+
+        map.clear();
+        map.addSwitch("uids");
+        assertEquals(map.toString(), "-uids");
+
+        // Test the get methods
+        map.clear();
+
+        // Ensure that the methods work properly when there is no such mapping
+        assertFalse(map.contains("none"));
+
+        assertNull(map.get("none"));
+        assertNull(map.getList("none"));
+        assertNull(map.getInteger("none"));
+        assertNull(map.getIntegerList("none"));
+
+        assertFalse(map.isList("none"));
+        assertFalse(map.isInteger("none"));
+        assertFalse(map.isSwitch("none"));
+
+        // Test a single key with a single value
+        map.add("entry", "value");
+        assertTrue(map.contains("entry"));
+        assertEquals(map.get("entry"), "value");
+        assertNull(map.getInteger("entry"));
+
+        assertFalse(map.isList("entry"));
+        assertFalse(map.isInteger("entry"));
+        assertFalse(map.isSwitch("entry"));
+
+        // Test a single key with multiple values
+        values = new String[] { "value1", "value2", "value3" };
+        for(String val : values) map.add("list", val);
+
+        assertTrue(map.isList("list"));
+        assertFalse(map.isInteger("list"));
+        assertFalse(map.isSwitch("list"));
+
+        assertNull(map.getInteger("list"));
+        assertEquals(map.get("list"), "value1");
+        assertEquals(map.getList("list"), Arrays.asList(values));
+
+        // Test integers
+        map.add("int", "123");
+        map.addInteger("int2", 1234);
+        assertTrue(map.isInteger("int"));
+        assertTrue(map.isInteger("int2"));
+
+        assertEquals(map.getInteger("int"), new Integer(123));
+        assertEquals(map.getInteger("int2"), new Integer(1234));
+
+        assertFalse(map.isList("int"));
+        assertFalse(map.isSwitch("int"));
+
+        // Test integer lists
+        Integer[] intVals = new Integer[] { 1, 2, 3, 4 };
+        for(Integer val : intVals) map.addInteger("intList", val);
+
+        assertTrue(map.isList("intList"));
+        assertEquals(map.getIntegerList("intList"), Arrays.asList(intVals));
+
+        // Test switches
+        map.addSwitch("switch");
+        assertTrue(map.isSwitch("switch"));
+        assertFalse(map.isList("switch"));
+        assertFalse(map.isInteger("switch"));
     }
 
 }
